0c06534dd6c19e657b8c7d3f92546b9436f16ed0
mccrodp <info@opensourceopenmind.ninja>
2018-02-06 20:26:47 +0900

    Swapping menus working but message / layout issue.

--- src/templates/general.js
+++ src/templates/general.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import Link from 'gatsby-link'
+import { getCurrentLangKey } from 'ptz-i18n'
+
+const ListLink = props =>
+  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+    <Link to={props.to}>
+      {props.children}
+    </Link>
+  </li>
+
+const ListLinkByLang = props =>
+  <ul style={{ listStyle: `none`, float: `right` }}>
+    <ListLink to={'/' + props.lang + '/'}>Home</ListLink>
+    <ListLink to={'/' + props.lang + '/about'}>About</ListLink>
+    <ListLink to={'/' + props.lang + '/schedule'}>Schedule</ListLink>
+  </ul>
 
 class GeneralTemplate extends React.Component {
   render () {
     const page = this.props.data.contentfulGeneral
 
+    const url = window.location.pathname
+    const { langs, defaultLangKey } = this.props.data.site.siteMetadata.languages
+    const langKey = getCurrentLangKey(langs, defaultLangKey, url)
+
     return (
       <div>
+        <ListLinkByLang lang={langKey} />
         <h1>{page.title}</h1>
         <div
           dangerouslySetInnerHTML={{
@@ -26,6 +47,14 @@ query generalQuery($id: String!) {
     body {
       childMarkdownRemark {
         html
+      }
+    }
+  }
+  site {
+    siteMetadata {
+      languages {
+        defaultLangKey
+        langs
       }
     }
   }
