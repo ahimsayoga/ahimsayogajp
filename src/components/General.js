ee674608b5ec00d12736b019000a1dfcebec0e23
mccrodp <info@opensourceopenmind.ninja>
2018-03-11 16:27:30 +0900

    Add Menu Component.

--- src/components/General.js
+++ src/components/General.js
@@ -3,7 +3,7 @@ import Link from 'gatsby-link'
 import * as PropTypes from 'prop-types'
 
 const propTypes = {
-  data: PropTypes.object.isRequired
+  node: PropTypes.object.isRequired
 }
 
 const General = ({ node }) => (
