bf2fa9261bd0167b60995d8621c78318a3cd3834
mccrodp <info@opensourceopenmind.ninja>
2018-03-29 17:38:16 +0900

    Added Wrapper component for contentful components (pages).

--- src/templates/landing.js
+++ src/templates/landing.js
@@ -1,12 +1,11 @@
 import React from 'react'
 
-import TextWrapper from '../components/TextWrapper'
+import * as contentfulWrapper from '../components/ContentfulWrapper'
 
 class LandingTemplate extends React.Component {
   render () {
     const page = this.props.data.contentfulLanding
 
-    console.log(page)
     return (
       <div>
         <h1>{page.title}</h1>
@@ -16,17 +15,14 @@ class LandingTemplate extends React.Component {
           }}
         />
           {
-            page.components.forEach((element) =>
-              <div>
-                <TextWrapper body={element.body} />
-              </div>
-            // console.log(element.body.childMarkdownRemark.html)
-              // <div
-              //   dangerouslySetInnerHTML={{
-              //     __html: element.body.childMarkdownRemark.html
-              //   }}
-              // />
-            )
+            page.components.map(( element ) => {
+              const componentName = element.parent.id;
+              const ContentfulWrapper = contentfulWrapper[componentName];
+
+              return (
+                <ContentfulWrapper key={element.id} component={element} />
+              );
+            })
           }
       </div>
     )
@@ -45,6 +41,7 @@ query landingQuery($id: String!) {
       }
     },
     components {
+      id
       parent {
         id
       }
